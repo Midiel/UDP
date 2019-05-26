c54ac2e5875e1f5321d18507c94e37b9e1f1ffc0
Midiel Rodriguez <midiel305@gmail.com>
2019-05-25 22:03:32 -0400

    Randomly drops packets, RTT, and allo client to timeout

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,14 +1,26 @@
 #UDPClient.py
 
-from socket import socket, SOCK_DGRAM, AF_INET
+#timeout import needed to catch timeout exception
+from socket import socket, SOCK_DGRAM, AF_INET, timeout
 
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# set timeout settings to 1.0 seconds
+clientSocket.settimeout(1.0)
+
 message = raw_input('Input lowercase sentence: ')
 clientSocket.sendto(message, (serverName, serverPort))
-modifiedMessage, addr = clientSocket.recvfrom(2048)
-print modifiedMessage, addr
+
+try:
+    modifiedMessage, addr = clientSocket.recvfrom(2048)
+    print (modifiedMessage, addr)
+
+# catch if timeout occurred
+except timeout as e:
+    print (e)
+     
 clientSocket.close()
 
 #Allow the client to give up if no response has been reveived within 1 second.
--- UDP/UDPServer.py
+++ UDP/UDPServer.py
@@ -1,5 +1,9 @@
 #UDPServer.py
 
+# To generate random number for the lost package part, and to calculate time
+import random, time
+
+
 #UDP (SOCK_DGRAM) is a datagram-based protocol. You send one 
 #datagram and get one reply and then the connection terminates.
 from socket import socket, SOCK_DGRAM, AF_INET
@@ -12,13 +16,27 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 print "Waiting for connections"
 while True:
+    # Start the clock
+    start = time.time()
+
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(2048)
     # Capitalize the message from the client
     print message, address
     message = message.upper()
-    serverSocket.sendto(message, address)
-serverSocket.close()
+
+    #Configure the server so that it randomly drops packets.
+    # Generate random number between 0 to 10
+    rand = random.randint(0, 10)
+    if rand < 3:
+        print("Ops!! Lost that packet.")
+    else:
+        serverSocket.sendto(message, address)
+
+    #Include information about how long each response took. This will be the RTT.
+    print("Response time (RTT): ", time.time()-start)
+
+serverSocket.close() 
 
 
 #Configure the server so that it randomly drops packets.
